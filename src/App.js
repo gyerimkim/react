095dadf12df503e7504c1db013039965a8dabd46
Gyerim Kim <gyerim.k@gmail.com>
2022-06-23 23:24:23 +0900

    useStateStudy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const[title, setTitle] = useState(
+   ["판교 맛집 추천",
+  "홍대 카페거리",
+"강남 옷수선 잘하는 곳"]
+ )
+
+ const [postingDay, setPostingDay] =useState([
+   "21년 10월 22일",
+   "22년 04월 19일",
+   "22년 06월 07일",
+ ]);
+
+ let [like, setLike] = useState(0);
+
+ return(
+   <div className='App'>
+     <div className='black-nav'>
+       <h4>겔미의 블로그</h4>
+     </div>
+     <button onClick={()=>
+     {
+       const sortTitle = [...title].sort();
+       setTitle(sortTitle);
+     }}>
+     가나다순 정렬</button>
+     <div className='list'>
+       <h4>
+         {title[0]}
+         <span onClick={()=>{
+           setLike(like++);
+         }}>👍</span>
+         {like}
+         {/* onClick안에는 함수 이름만 넣어야함 */}
+       </h4>
+       <p>{postingDay[0]}</p>
+     </div>
+     <div className='list'>
+       <h4>
+         {title[1]}
+         <span onClick={()=>{
+           const changeTitle = [...title];
+           changeTitle[1]='마곡 카페거리';
+           setTitle(changeTitle);
+         }}>⏩</span>
+       </h4>
+         <p>{postingDay[1]}</p>
+     </div>
+         <div className='list'>
+           <h4>{title[2]}</h4>
+           <p>{postingDay}</p>
+         </div>
+   </div>
+ )
+
 }
 
 export default App;
